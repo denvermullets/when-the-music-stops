a5b3f041e07785537eb5e34bb187181b2ae10260
denvermullets <devmullets@gmail.com>
2024-01-26 09:47:05 -0500

    feat(pagy): add pagination

--- app/controllers/sub_forums_controller.rb
+++ app/controllers/sub_forums_controller.rb
@@ -1,7 +1,9 @@
 class SubForumsController < ApplicationController
+  include Pagy::Backend
+
   def show
     sub_forum = SubForum.find_by(slug: params[:sub_forum_slug])
-    topics = Topic.where(sub_forum_id: sub_forum.id)
+    @pagy, topics = pagy(Topic.where(sub_forum_id: sub_forum.id))
 
     render :show, locals: { topics:, sub_forum: }
   end
