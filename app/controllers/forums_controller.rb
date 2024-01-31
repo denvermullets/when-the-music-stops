16e87cbbdc3beaccd6088b961d8c63209644e0f9
denvermullets <devmullets@gmail.com>
2024-01-31 08:11:00 -0500

    feat(activity): update index view

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,7 +1,16 @@
 class ForumsController < ApplicationController
   def index
     forums = Forum.all
+    last_comment = Comment.last
+    last_topic = Topic.last
 
-    render :index, locals: { forums: }
+    # this will only be nil if it's a fresh wtms instance
+    last_post = if last_topic.nil?
+                  nil
+                else
+                  last_comment.created_at > last_topic.created_at ? last_comment : last_topic
+                end
+
+    render :index, locals: { forums:, last_post: }
   end
 end
