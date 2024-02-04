5bdbcf1b611a25189993f09d918d975bc9a61f59
denvermullets <devmullets@gmail.com>
2024-02-04 13:04:04 -0500

    fix(subForum): add order to sub_forums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_122515) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_175534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_122515) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "forum_order", default: 0
     t.index ["forum_id"], name: "index_sub_forums_on_forum_id"
     t.index ["slug"], name: "index_sub_forums_on_slug", unique: true
   end
