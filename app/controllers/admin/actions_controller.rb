a45f98a0abd9872a0019f6b8f4bc08fe84d7460e
denvermullets <devmullets@gmail.com>
2024-11-28 10:13:50 -0500

    fix(admin): prevent deleting current_user

--- app/controllers/admin/actions_controller.rb
+++ app/controllers/admin/actions_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ActionsController < ApplicationController
   def delete_user
+    return if params[:user_id].to_i == current_user.id
+
     DeleteUser::History.call(user_id: params[:user_id])
 
     redirect_to root_path
